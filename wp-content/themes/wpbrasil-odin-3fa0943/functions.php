4b7cebace32d0cb1a32394961fefc195d31b84e6
richardeixo <richard@eixo.digital>
2021-08-31 15:55:50 -0300

    func mob-desk

--- wp-content/themes/wpbrasil-odin-3fa0943/functions.php
+++ wp-content/themes/wpbrasil-odin-3fa0943/functions.php
@@ -16,6 +16,20 @@
 /**
  * Sets content width.
  */
+ 
+ $device = 0;
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+
+if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+{
+    $device = "mobile";
+ }else {
+    $device = "desktop";
+ }
 if ( ! isset( $content_width ) ) {
 	$content_width = 600;
 }
